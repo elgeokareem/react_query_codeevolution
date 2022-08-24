3f4bce1222cca054bd4011960bffaa8dfc7829e8
william vegas <elkareem123@gmail.com>
2022-08-23 21:34:51 -0400

    type of the useQuery hook and success and error callbacks

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -1,21 +1,30 @@
 import { useQuery } from "react-query";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 import type { SuperHeroesType } from "../types";
 
 export default function RQSuperHeroesPage() {
-  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
-    "super-heroes",
-    fetchSuperHeroes,
-    {
-      // cacheTime: 5000 // Cuanto tiempo estará en cache la data
-      // staleTime: 30000, // Por cuanto tiempo no hace falta refetch (fresh time)
-      // refetchOnMount: true, // Si se monta el componente, se refetcha
-      // refetchOnWindowFocus: true // Si se pone el foco en la ventana, se refetcha
-      // refetchInterval: 2000 // Cada cuanto se refetcha
-      // refetchIntervalInBackground: true // refetch incluso si no esta en focus
-    }
-  );
+  function onSuccess(data: SuperHeroesType[]) {
+    console.log("side effect after data fetching success", data);
+  }
+
+  function onError(error: AxiosError) {
+    console.log("side effect after error", error);
+  }
+
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery<
+    SuperHeroesType[],
+    AxiosError
+  >("super-heroes", fetchSuperHeroes, {
+    // cacheTime: 5000 // Cuanto tiempo estará en cache la data
+    // staleTime: 30000, // Por cuanto tiempo no hace falta refetch (fresh time)
+    // refetchOnMount: true, // Si se monta el componente, se refetcha
+    // refetchOnWindowFocus: true // Si se pone el foco en la ventana, se refetcha
+    // refetchInterval: 2000 // Cada cuanto se refetcha
+    // refetchIntervalInBackground: true // refetch incluso si no esta en focus
+    onSuccess,
+    onError
+  });
 
   if (isLoading) {
     return <h2>Loading...</h2>;
@@ -29,13 +38,14 @@ export default function RQSuperHeroesPage() {
     <>
       <h2>RQ Super Heroes Page</h2>
       <button onClick={() => refetch()}>fetch HEROS</button>
-      {data?.data.map(hero => {
+      {data?.map(hero => {
         return <div key={hero.id}>{hero.name}</div>;
       })}
     </>
   );
 }
 
-function fetchSuperHeroes() {
-  return axios.get<SuperHeroesType[]>("http://localhost:4000/superheroes");
+async function fetchSuperHeroes() {
+  const res = await axios.get("http://localhost:4000/superheroes");
+  return res.data;
 }
