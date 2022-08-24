461c2439f20d7a0ac641ba85d24f64b395a8017e
william vegas <elkareem123@gmail.com>
2022-08-23 21:59:36 -0400

    select property to modify the data

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -12,19 +12,21 @@ export default function RQSuperHeroesPage() {
     console.log("side effect after error", error);
   }
 
-  const { isLoading, data, isError, error, isFetching, refetch } = useQuery<
-    SuperHeroesType[],
-    AxiosError
-  >("super-heroes", fetchSuperHeroes, {
-    // cacheTime: 5000 // Cuanto tiempo estará en cache la data
-    // staleTime: 30000, // Por cuanto tiempo no hace falta refetch (fresh time)
-    // refetchOnMount: true, // Si se monta el componente, se refetcha
-    // refetchOnWindowFocus: true // Si se pone el foco en la ventana, se refetcha
-    // refetchInterval: 2000 // Cada cuanto se refetcha
-    // refetchIntervalInBackground: true // refetch incluso si no esta en focus
-    onSuccess,
-    onError
-  });
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes,
+    {
+      // cacheTime: 5000 // Cuanto tiempo estará en cache la data
+      // staleTime: 30000, // Por cuanto tiempo no hace falta refetch (fresh time)
+      // refetchOnMount: true, // Si se monta el componente, se refetcha
+      // refetchOnWindowFocus: true // Si se pone el foco en la ventana, se refetcha
+      // refetchInterval: 2000 // Cada cuanto se refetcha
+      // refetchIntervalInBackground: true // refetch incluso si no esta en focus
+      onSuccess,
+      onError,
+      select: data => data.map((hero: SuperHeroesType) => hero.name)
+    }
+  );
 
   if (isLoading) {
     return <h2>Loading...</h2>;
@@ -38,8 +40,11 @@ export default function RQSuperHeroesPage() {
     <>
       <h2>RQ Super Heroes Page</h2>
       <button onClick={() => refetch()}>fetch HEROS</button>
-      {data?.map(hero => {
+      {/*data without select {data?.map(hero => {
         return <div key={hero.id}>{hero.name}</div>;
+      })} */}
+      {data?.map((heroName, index) => {
+        return <div key={index}>{heroName}</div>;
       })}
     </>
   );
