e97f99a5cb982b01abc6a6a5c9aa684dc3bfef96
William Vegas <elkareem123@gmail.com>
2022-09-25 00:33:18 -0400

    22. query invalidation, updating when mutating data with http requests

--- src/hooks/useSuperHeroesData.ts
+++ src/hooks/useSuperHeroesData.ts
@@ -4,9 +4,15 @@ import {
   useMutation,
   useQueryClient
 } from "react-query";
+<<<<<<< HEAD
 import axios, { AxiosError } from "axios";
+=======
+import axios, { AxiosError, AxiosResponse } from "axios";
+>>>>>>> 5ec881a (22. query invalidation, updating when mutating data with http requests)
 
 import type { SuperHeroesType, AddSuperHero } from "../types";
+
+type lol = SuperHeroesType[] | undefined;
 
 async function fetchSuperHeroes() {
   const res = await axios.get("http://localhost:4000/superheroes");
@@ -22,9 +28,21 @@ const addSuperHero = (hero: AddSuperHero) => {
 export const useAddSuperHeroData = () => {
   const queryClient = useQueryClient();
   return useMutation(addSuperHero, {
+<<<<<<< HEAD
     onSuccess: () => {
       // the key is the same as the one used in useSuperHeroesData
       queryClient.invalidateQueries("super-heroes");
+=======
+    onSuccess: (hero: AxiosResponse<SuperHeroesType[]>) => {
+      // the key is the same as the one used in useSuperHeroesData
+      // queryClient.invalidateQueries("super-heroes");
+
+      // This is another way for doing the same thing above
+      // but with this method we save an extra http get request
+      queryClient.setQueryData("super-heroes", (oldQueryData: any) => {
+        return [...oldQueryData, hero.data];
+      });
+>>>>>>> 5ec881a (22. query invalidation, updating when mutating data with http requests)
     }
   });
 };
